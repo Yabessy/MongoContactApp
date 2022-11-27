7ad387cc2bcec89400745ccb62b4e05f71029635
JohanLiebert <johanliebert.outergod@gmail.com>
2022-11-27 07:32:52 +0700

    i giveup lol i will do it in another project

--- app.js
+++ app.js
@@ -71,13 +71,6 @@ app.post(
     check("noHp", "noHp Tidak Valid").isMobilePhone("id-ID"),
   ],
   (req, res) => {
-    const scriptURL ="https://script.google.com/macros/s/AKfycbxbVC7zhbrE_t_rNeeDiL52b5hk1uBsQI26251SZoAd1_VVe0zEYeZ8WFV0vAMuLRlO/exec";
-    const form = document.forms["submit-to-google-sheet"];
-    form.addEventListener("submit", (e) => {
-      fetch(scriptURL, { method: "POST", body: new FormData(form) })
-        .then((response) => console.log("Success!", response))
-        .catch((error) => console.error("Error!", error.message));
-    });
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
       res.render("add-contact", {
