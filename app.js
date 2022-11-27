f4c0ca0b334d07fea9be3f2117f42093501b095e
JohanLiebert <johanliebert.outergod@gmail.com>
2022-11-27 07:11:33 +0700

    add submit to spreadsheet attempt 2

--- app.js
+++ app.js
@@ -81,9 +81,7 @@ app.post(
     } else {
       const scriptURL = "https://script.google.com/macros/s/AKfycbxbVC7zhbrE_t_rNeeDiL52b5hk1uBsQI26251SZoAd1_VVe0zEYeZ8WFV0vAMuLRlO/exec";
       const form = document.forms["submit-to-google-sheet"];
-        fetch(scriptURL, { method: "POST", body: new FormData(form) })
-          .then((response) => console.log("Success!", response))
-          .catch((error) => console.error("Error!", error.message));
+      fetch(scriptURL, { method: "POST", body: new FormData(form) })
       ContactModel.insertMany(req.body, (error, result) => {
         req.flash("msg", "Data Contact Berhasil ditambahkan");
         res.redirect("/contact");
