4af41620e25b7a1bf8e07bc413c195f8dce88c7b
JohanLiebert <johanliebert.outergod@gmail.com>
2022-11-27 07:18:49 +0700

    add to spreadsheet attempt3

--- app.js
+++ app.js
@@ -79,9 +79,6 @@ app.post(
         errors: errors.array(),
       });
     } else {
-      const scriptURL = "https://script.google.com/macros/s/AKfycbxbVC7zhbrE_t_rNeeDiL52b5hk1uBsQI26251SZoAd1_VVe0zEYeZ8WFV0vAMuLRlO/exec";
-      const form = document.forms["submit-to-google-sheet"];
-      fetch(scriptURL, { method: "POST", body: new FormData(form) })
       ContactModel.insertMany(req.body, (error, result) => {
         req.flash("msg", "Data Contact Berhasil ditambahkan");
         res.redirect("/contact");
