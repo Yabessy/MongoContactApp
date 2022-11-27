8bcd603476dc545a30cec664ccffd287271a4a42
JohanLiebert <johanliebert.outergod@gmail.com>
2022-11-27 07:30:08 +0700

    add to spreadsheet attempt 4

--- utils/SubmitToSpreadsheet.js
+++
@@ -1,9 +0,0 @@
-const scriptURL = "https://script.google.com/macros/s/AKfycbxbVC7zhbrE_t_rNeeDiL52b5hk1uBsQI26251SZoAd1_VVe0zEYeZ8WFV0vAMuLRlO/exec";
-const form = document.forms["submit-to-google-sheet"];
-
-form.addEventListener("submit", (e) => {
-  e.preventDefault();
-  fetch(scriptURL, { method: "POST", body: new FormData(form) })
-    .then((response) => console.log("Success!", response))
-    .catch((error) => console.error("Error!", error.message));
-});
